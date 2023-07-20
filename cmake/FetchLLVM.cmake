1dd052f266ffd49e885dbaa37349d1deaff64643
Lu, Chengjun <chengjun.lu@intel.com>
2023-07-20 09:45:17 +0000

    Add alternative LLVM revision for XPU backend

---
+++ cmake/FetchLLVM.cmake
@@ -0,0 +1,9 @@
+
+include(FetchContent)
+
+
+# TODO: Download llvm release package from the llvm release site.
+
+set(LLVM_DIR "/home/chengjun/workspace/CLionProject/triton-llvm/llvm/build/")
+set(LLVM_LIBRARY_DIR "/home/chengjun/workspace/CLionProject/triton-llvm/llvm/build/lib")
+set(LLVM_INCLUDE_DIRS "/home/chengjun/workspace/CLionProject/triton-llvm/llvm/build/include")
